b6f5b4ad67cec8c22d82d44170ac2129c3a8e16f
Nick <gamezo90@mail.ru>
2022-10-19 20:17:33 +0300

    Add UserRepository and UserController

---
+++ api/src/main/java/com/rentcar/controller/dto/UserController.java
@@ -0,0 +1,38 @@
+package com.rentcar.controller.dto;
+
+
+import com.rentcar.repository.UserRepository;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.validation.Valid;
+import java.security.Principal;
+import java.util.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/rest/data/users")
+public class UserController {
+
+    private final UserRepository repository;
+
+    @GetMapping
+    public ResponseEntity<Object> findAll() {
+
+        return new ResponseEntity<>(
+                Collections.singletonMap("result", repository.findAll()),
+                HttpStatus.OK
+        );
+    }
+}
---
+++ common/src/main/java/com/rentcar/repository/UserRepository.java
@@ -0,0 +1,46 @@
+package com.rentcar.repository;
+
+import com.rentcar.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findAll();
+
+//    User findByIdAndGender(Long id, Gender gender);
+
+//    HibernateUser findByCredentialsLogin(String login);
+
+//    List<HibernateUser> findByCredentialsLoginAndUserNameAndBirth(String login, String name, Timestamp birthDate);
+
+//    List<HibernateUser> findByCredentialsLoginAndUserNameOrBirthOrderByIdDescUserNameDesc(String login, String name, Timestamp birthDate);
+
+//    List<HibernateUser> findByIsDeletedOrderByIdDesc(Boolean isDeleted);
+
+    //select * from m_users where (login = ? and name = ?) or birth_date = ?
+//
+//    @Query(value = "select u from HibernateUser u")
+//    List<HibernateUser> findByHQLQuery();
+
+//    @Query(value = "select * from carshop.users", nativeQuery = true)
+//    List<HibernateUser> findByHQLQueryNative();
+
+//    @Query(value = "select u from HibernateUser u where u.credentials.login = :login and u.userName = :userName")
+//    List<HibernateUser> findByHQLQuery(String login, @Param("userName") String name);
+//
+//    @Query("select u.id, u.userName from HibernateUser u")
+//    List<Object[]> getPartsOfUser();
+
+//
+//    @Modifying
+//    @Query(value = "insert into carshop.l_role_user(user_id, role_id) values (:user_id, :role_id)", nativeQuery = true)
+//    int createRoleRow(@Param("user_id") Long userId, @Param("role_id") Long roleId);
+}
