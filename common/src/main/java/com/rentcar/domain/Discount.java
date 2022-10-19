ff31b3d802d36e07e2405b2b65a094048c70c344
Nick <gamezo90@mail.ru>
2022-10-19 18:47:27 +0300

    Change Discount

--- common/src/main/java/com/rentcar/domain/Discount.java
+++ common/src/main/java/com/rentcar/domain/Discount.java
@@ -17,25 +17,25 @@ import java.sql.Timestamp;
 @Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Discount {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Column(name = "user_id")
-        private Long userId;
+    @Column(name = "user_id")
+    private Long userId;
 
-        @Column(name = "discount_size")
-        private Float discountSize;
+    @Column(name = "discount_size")
+    private Float discountSize;
 
-        @Column(name = "creation_date")
-        @JsonIgnore
-        private Timestamp creationDate;
+    @Column(name = "creation_date")
+    @JsonIgnore
+    private Timestamp creationDate;
 
-        @Column(name = "modification_date")
-        @JsonIgnore
-        private Timestamp modificationDate;
+    @Column(name = "modification_date")
+    @JsonIgnore
+    private Timestamp modificationDate;
 
-        @Column(name = "expiration_date")
-        private Timestamp expirationDate;
+    @Column(name = "expiration_date")
+    private Timestamp expirationDate;
 }
 
