912239141365649949ee9d3e83e38f233044f9eb
Nick <gamezo90@mail.ru>
2022-10-19 18:45:01 +0300

    Add Discount and change db

---
+++ common/src/main/java/com/rentcar/domain/Discount.java
@@ -0,0 +1,41 @@
+package com.rentcar.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.cache.annotation.Cacheable;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Data
+@Entity
+@Table(name = "discount_system")
+@Cacheable("discount_system")
+@javax.persistence.Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Discount {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @Column(name = "user_id")
+        private Long userId;
+
+        @Column(name = "discount_size")
+        private Float discountSize;
+
+        @Column(name = "creation_date")
+        @JsonIgnore
+        private Timestamp creationDate;
+
+        @Column(name = "modification_date")
+        @JsonIgnore
+        private Timestamp modificationDate;
+
+        @Column(name = "expiration_date")
+        private Timestamp expirationDate;
+}
+
---
+++ common/src/main/resources/db/migration/V1_5__change_car_id_in_order_history.sql
@@ -0,0 +1,2 @@
+alter table rentcar.order_history
+    rename column cars_id to car_id;
